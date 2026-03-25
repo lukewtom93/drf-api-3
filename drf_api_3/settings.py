ab2019fb495d51c8c33eaef557581a1167c99e8d
lukewtom93 <lukewtom93@gmail.com>
2026-03-25 13:49:21 +0000

    prepared for deployment

--- drf_api_3/settings.py
+++ drf_api_3/settings.py
@@ -95,7 +95,6 @@ INSTALLED_APPS = [
 SITE_ID = 1
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
