1da962af471fb5a824380b62e5fd66055796b046
lukewtom93 <lukewtom93@gmail.com>
2026-03-25 09:15:17 +0000

    fixed put request to media server

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 import os
 os.environ['CLOUDINARY_URL'] = (
-	'cloudinary://<939297373232796>:<ypDPPRXmk9LK7LuWnNCHRUYmzM8>@diphfoons'
+	'cloudinary://939297373232796:ypDPPRXmk9LK7LuWnNCHRUYmzM8@diphfoons'
 )
 
