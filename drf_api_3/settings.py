413d49c2f5cf8521b27d2be48a292274a53afab8
lukewtom93 <lukewtom93@gmail.com>
2026-03-27 14:49:40 +0000

    settings changed

--- drf_api_3/settings.py
+++ drf_api_3/settings.py
@@ -36,7 +36,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
-if 'DEV' not in os.environ:
+if 'DEV' in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
     ]
