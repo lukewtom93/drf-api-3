612ac1cb4893d9bce16bd6b3804814a041262d4c
lukewtom93 <lukewtom93@gmail.com>
2026-03-27 14:42:55 +0000

    settings changed

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ os.environ['CLOUDINARY_URL'] = (
 	'cloudinary://939297373232796:ypDPPRXmk9LK7LuWnNCHRUYmzM8@diphfoons'
 )
 os.environ.setdefault('SECRET_KEY', 'p102<%k}8Tz4]@EZTyn%.PT|+3=UFo')
-# os.environ['DEV'] = '1'
+os.environ['DEV'] = '1'
 os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:ldiOFb8Ir7Ua@ep-dark-poetry-a2g5vf2y.eu-central-1.aws.neon.tech/city_chew_trek_535918'
